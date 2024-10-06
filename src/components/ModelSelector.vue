3ab1b47655c8150ce6d4a74713354f468bcabd32
OreaSync <126176391+OreaSync@users.noreply.github.com>
2024-10-06 14:17:08 +0300

    Added Animations

--- src/components/ModelSelector.vue
+++ src/components/ModelSelector.vue
@@ -1,23 +1,14 @@
 <template>
-  <div class="flex justify-center items-center mb-4">
+  <div class="flex justify-center items-center" ref="dropDownAnim">
     <nav>
       <ul class="relative group" @mouseleave="animateDropdownOut">
         <li>
-          <a
-            class="cursor-pointer px-4 py-2 rounded-xl border-2 bg-[#111111] border-[#1b1b1bec]"
-            @mouseenter="animateDropdown"
-          >
+          <a class="cursor-pointer px-4 py-2 rounded-xl border-2 bg-[#111111] border-[#1b1b1bec]" @mouseenter="animateDropdown">
             Models
           </a>
-          <ul
-            class="absolute left-0 hidden group-hover:block rounded-md pt-2 w-[180px]"
-            ref="Selector"
-          >
+          <ul class="absolute left-0 hidden group-hover:block rounded-md pt-2 w-[180px]" ref="Selector">
             <li v-for="model in models" :key="model.name">
-              <a
-                @click="handleClick(model.name, $event)"
-                class="flex justify-between items-center cursor-pointer px-2 py-2 rounded-xl border-2 bg-[#111111] border-[#1b1b1bec] mb-1"
-              >
+              <a @click="handleClick(model.name, $event)" class="flex justify-between items-center cursor-pointer px-2 py-2 rounded-xl border-2 bg-[#111111] border-[#1b1b1bec] mb-1">
                 {{ model.name }}
                 <span v-if="model.name === selectedModel"><Icon icon="iconamoon:check-bold" width="1.2em" height="1.2em" /></span>
               </a>
@@ -30,10 +21,11 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import gsap from 'gsap';
 
 const Selector = ref(null);
+const dropDownAnim = ref(null);
 
 const models = [
   { name: 'GPT3.5 Turbo' },
@@ -82,4 +74,8 @@ function selectModel(model) {
   selectedModel.value = model;
   emit('selectModel', model);
 }
+
+onMounted(() => {
+  gsap.fromTo(dropDownAnim.value, { opacity: 0 }, { opacity: 1, y: 12, duration: 1 })
+});
 </script>
--- src/components/QuestionArea.vue
+++ src/components/QuestionArea.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Тута кароче строка ввода -->
-    <div class="fixed bottom-0 left-0 right-0 flex gap-2 justify-center p-4">
+    <div class="fixed bottom-0 left-0 right-0 flex gap-2 justify-center p-4 mb-4" ref="Area">
       <textarea
         required
         v-model="inputValue"
@@ -9,31 +9,33 @@
         class="border-2 opacity-50 focus:opacity-100 hover:opacity-100 overflow-y-hidden transition ease-in-out delay-250 bg-[#111111] border-[#1b1b1bec] focus:outline-none rounded resize-none w-full md:w-2/3 lg:w-3/4 xl:w-2/5 px-2 py-1"
         rows="1"
         />
-      <button :disabled="sendTimeout" @click="sendMessage" class="bg-blue-500 text-white focus:outline-none px-[9.5px] py-1 rounded">
+      <button ref="SendButton" :disabled="sendTimeout" @click="sendMessage" class="bg-indigo-700 text-white focus:outline-none px-[9.5px] py-1 rounded">
         <Icon icon="mingcute:send-line" width="1.4em" height="1.4em" />
       </button>
     </div>
   </template>
   
   <script setup>
-  import { ref } from 'vue';
+  import { ref, onMounted } from 'vue';
   import { Icon } from '@iconify/vue';
   import axios from 'axios';
+  import gsap from 'gsap';
   
   const props = defineProps(['selectedModel', 'link', 'messages']);
   const emit = defineEmits(['updateMessages']);
   
   const inputValue = ref('');
   const sendTimeout = ref(false);
+  const Area = ref(null);
+  const SendButton = ref(null);
   
   async function sendMessage() {
     if (sendTimeout.value || inputValue.value.trim() === '') return;
-  
+
     const newMessage = {
       role: 'user',
       content: inputValue.value
     };
-  
     emit('updateMessages', newMessage);
   
     sendTimeout.value = true;
@@ -44,33 +46,19 @@
         dataToSend = [{ role: 'user', content: inputValue.value }];
         break;
       case 'Blackbox':
-        dataToSend = {
-          model: 'blackbox',
-          request: {
-            messages: [{ role: 'user', content: inputValue.value }]
-          }
-        };
+        dataToSend = {model: 'blackbox', request: {messages: [{ role: 'user', content: inputValue.value }]}};
         break;
       case 'Gemini Pro':
-        dataToSend = {
-          model: 'gemini-pro',
-          request: {
-            messages: [{ role: 'user', content: inputValue.value }]
-          }
-        };
+        dataToSend = {model: 'gemini-pro', request: {messages: [{ role: 'user', content: inputValue.value }]}};
         break;
       case 'Gemini':
-        dataToSend = {
-          model: 'gemini',
-          request: {
-            messages: [{ content: inputValue.value }]
-          }
-        };
+        dataToSend = {model: 'gemini', request: {messages: [{ content: inputValue.value }]}};
         break;
       default:
         dataToSend = [{ role: 'user', content: inputValue.value }];
     }
     inputValue.value = '';
+
     try {
       const response = await axios.post(props.link, dataToSend);
       const aiResponse = response.data.answer;
@@ -83,5 +71,8 @@
       }, 5000);
     }
   }
+onMounted(() => {
+  gsap.fromTo(Area.value, { opacity: 0 }, { opacity: 1, y: 16, duration: 1 });
+})
   </script>
   
