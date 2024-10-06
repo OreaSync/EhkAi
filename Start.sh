3a655f1e3da50a3585f1a607afb8d26a313bec73
OreaSync <126176391+OreaSync@users.noreply.github.com>
2024-10-06 14:34:18 +0300

    Update start.sh

--- Start.sh
+++ Start.sh
@@ -2,6 +2,7 @@
 
 sudo apt update && sudo apt upgrade
 sudo apt-get install curl
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
 sudo apt-get install nodejs
 npm install 
 npm run dev
